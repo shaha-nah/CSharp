734e97b4ac656b38247b861891ca467133697a75
super-hxman <shahanahbp@gmail.com>
2021-07-19 23:23:23 +0400

    Generics

---
+++ Generics/Generics/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generics
+{
+    //types of contraints: 
+    // where T : IComparable - apply constraint to interface
+    // where T : Product - apply constraing to class + subclasses
+    // where T : struct - value type
+    // where T : class - reference type
+    // where T : new() - object with default constructor
+    
+    public class Utilities<T> where T : IComparable, new()
+    {
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public void DoSomething(T value)
+        {
+            var obj = new T();
+        }
+
+        public T Max(T a, T b)
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
