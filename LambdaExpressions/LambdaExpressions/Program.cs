6ce448bd35053877ddd25cff41ddd0a48cf8a583
super-hxman <shahanahbp@gmail.com>
2021-07-27 20:44:26 +0400

    LambdaExpressions

---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //args => expression
+
+            //() => ...
+            //x => ...
+            //(x,y, z) => ...
+
+            //Func<int, int> : delegate
+            // Func<int, int> square = number => number * number;
+            // Console.WriteLine(square(5));
+
+            //method that takes a number and multiplies that by its factor
+            // const int factor = 5;
+            // Func<int, int> mutiplier = n => n * factor;
+            // var result = multiplier(10);
+            // Console.WriteLine(result);
+
+            var books = new BookRepository().GetBooks();
+
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+    }
+}
