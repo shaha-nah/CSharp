97e27faf3cadea6549193856b0cec3f9150a0288
super-hxman <shahanahbp@gmail.com>
2021-07-28 15:12:55 +0400

    Nullable types

--- NullableTypes/NullableTypes/Program.cs
+++ NullableTypes/NullableTypes/Program.cs
@@ -8,6 +8,17 @@ namespace NullableTypes
         static void Main(string[] args)
         {
             DateTime? date = null;
+
+            // Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault()); //preferred
+            // Console.WriteLine("HasValue: " + data.HasValue);
+            // Console.WriteLine("Value" + data.Value);
+
+            // DateTime? date = new DateTime(2014, 1, 1);
+            // DateTime date2 = date.GetValueOrDefault();
+            // DateTime? date3 = date2; //a value type can be converted to a nullable type
+            // Console.WriteLine(date3.GetValueOrDefault());
+
+
             DateTime date2 = date ?? DateTime.Today;
 
             DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
