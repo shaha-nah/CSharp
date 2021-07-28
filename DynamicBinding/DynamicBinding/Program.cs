2f11413fadae3604bf42ca60d72bd17cc41294d3
super-hxman <shahanahbp@gmail.com>
2021-07-28 15:25:37 +0400

    Dynamic binding

---
+++ DynamicBinding/DynamicBinding/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Threading;
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //* Reflection is a way to inspect the metadata about the type
+            object obj = "boo";
+            // obj.GetHashCode();
+            
+            //* with reflection
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+
+            //* with dynamic
+            dynamic excelObject = "dummy";
+            excelObject.Optimize();
+
+            //* can change types -- no explicit casting
+            dynamic name = "boo";
+            // name = 10;
+            // name++; //!exception since we cannot do that with a string
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b; //*c will become dynamic
+
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+
+
+        }
+    }
+}
